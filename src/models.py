f4b08da68257754d3ad0478ca9c8f72bf56b9387
williamyang1991 <596836482@qq.com>
2020-09-10 21:32:45 +0800

    Update models.py

--- src/models.py
+++ src/models.py
@@ -371,10 +371,10 @@ class DiscriminatorSN(nn.Module):
         self.useFC = useFC
         
         # 32*32 --> 16*16 --> 8*8
-        size = input_size/2
+        size = input_size//2
         nf_mult = 1
         for n in range(1, n_layers):
-            size = size / 2
+            size = size // 2
             nf_mult_prev = nf_mult
             nf_mult = min(2 ** n, 8)
             modelList.append(ReflectionPad2d(padding=padding))
